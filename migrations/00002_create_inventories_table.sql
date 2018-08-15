d02e972f379365ec4efecbcd68649536bce586b4
Ben Lu <coopermilk123@gmail.com>
2018-08-15 11:03:00 -0700

    Remove unused columns

--- migrations/00002_create_inventories_table.sql
+++ migrations/00002_create_inventories_table.sql
@@ -2,8 +2,6 @@
 CREATE TABLE inventories (
     inventory_id int NOT NULL AUTO_INCREMENT,
     buyer_id int,
-    token_id text,
-    category int,
     price int NOT NULL DEFAULT '0',
     metadata int NOT NULL DEFAULT '0',
     buy_time int(11),
