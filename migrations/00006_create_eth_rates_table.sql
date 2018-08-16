6cd17fbe9ec2c9590595cacd5a59fe6eed913f54
Ben Lu <coopermilk123@gmail.com>
2018-08-16 14:33:13 -0700

    Add time column to eth_rates

--- migrations/00006_create_eth_rates_table.sql
+++ migrations/00006_create_eth_rates_table.sql
@@ -3,8 +3,9 @@ CREATE TABLE eth_rates (
     id int NOT NULL AUTO_INCREMENT,
     symbol varchar(10) NOT NULL,
     price decimal(18, 2) NOT NULL DEFAULT '0',
+    time int(11) NOT NULL,
     PRIMARY KEY(id),
-    KEY (symbol)
+    KEY (symbol, time)
 );
 
 -- +goose Down
