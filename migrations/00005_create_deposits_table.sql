ac47ccfa575ee0c2750347541b49d6c21d873414
Ben Lu <coopermilk123@gmail.com>
2018-08-17 09:27:21 -0700

    Change table names

--- migrations/00005_create_deposits_table.sql
+++ migrations/00005_create_deposits_table.sql
@@ -1,12 +1,12 @@
 -- +goose Up
-CREATE TABLE deposit_logs (
-    log_id int NOT NULL AUTO_INCREMENT,
+CREATE TABLE deposits (
+    deposit_id int NOT NULL AUTO_INCREMENT,
     txhash varchar(80) NOT NULL,
     address varchar(60) NOT NULL,
     approved tinyint(1) NOT NULL DEFAULT '0',
-    PRIMARY KEY(log_id),
+    PRIMARY KEY(deposit_id),
     KEY (txhash, address)
 );
 
 -- +goose Down
-DROP TABLE deposit_logs;
+DROP TABLE deposits;
--- migrations/00007_create_payments_table.sql
+++ migrations/00007_create_payments_table.sql
@@ -1,16 +1,18 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
-CREATE TABLE transaction_logs (
-    transaction_log_id int NOT NULL AUTO_INCREMENT,
+CREATE TABLE payments (
+    payment_id int NOT NULL AUTO_INCREMENT,
     inventory_id int,
     buyer_id int,
     buy_time int(11),
-    PRIMARY KEY(transaction_log_id),
+    check_in_time int(11),
+    token_id int(11),
+    PRIMARY KEY(payment_id),
     FOREIGN KEY (buyer_id) REFERENCES users(user_id),
     FOREIGN KEY (inventory_id) REFERENCES inventories(inventory_id),
-    KEY (buy_time)
+    KEY (buy_time, check_in_time, token_id)
 );
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
 -- +goose Down
-DROP TABLE transaction_logs;
+DROP TABLE payments;
