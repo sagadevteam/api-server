e41e50784d166a450cdda97745efbf77f7e52490
Ben Lu <coopermilk123@gmail.com>
2018-08-17 10:28:10 -0700

    Add room_no, title and description columns to inventories table

--- migrations/00002_create_inventories_table.sql
+++ migrations/00002_create_inventories_table.sql
@@ -6,8 +6,11 @@ CREATE TABLE inventories (
     start_time int(11) NOT NULL,
     end_time int(11) NOT NULL,
     created_time int(11) NOT NULL,
+    room_no varchar(20) NOT NULL,
+    title text NOT NULL,
+    description text NOT NULL,
     PRIMARY KEY(inventory_id),
-    KEY (price, metadata, start_time, end_time, created_time)
+    KEY (price, metadata, start_time, end_time, created_time, room_no)
 );
 
 -- +goose Down
