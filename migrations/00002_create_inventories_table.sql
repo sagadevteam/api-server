653a27928280d4708664fb66534a87e45098b5c3
Ben Lu <coopermilk123@gmail.com>
2018-08-17 10:18:28 -0700

    Remove buyer_id from inventories table

--- migrations/00002_create_inventories_table.sql
+++ migrations/00002_create_inventories_table.sql
@@ -1,7 +1,6 @@
 -- +goose Up
 CREATE TABLE inventories (
     inventory_id int NOT NULL AUTO_INCREMENT,
-    buyer_id int,
     price int NOT NULL DEFAULT '0',
     metadata int NOT NULL DEFAULT '0',
     start_time int(11) NOT NULL,
