4e06e5cba0bc0039b4d26689678ef18e42ee1be7
sc0Vu <alk03073135@gmail.com>
2018-08-17 17:30:15 -0700

    Add govalidator package

--- requests/loginRequest.go
+++ requests/loginRequest.go
@@ -2,6 +2,6 @@ package requests
 
 // LoginRequest is login schema in post form
 type LoginRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string `json:"email" valid:"email,required"`
+	Password string `json:"password" valid:"matches([a-zA-Z0-9]+),runelength(6|15),required"`
 }
--- requests/signupRequest.go
+++ requests/signupRequest.go
@@ -2,6 +2,7 @@ package requests
 
 // SignupRequest is signup schema in post form
 type SignupRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email         string `json:"email" valid:"email,required"`
+	Password      string `json:"password" valid:"matches([a-zA-Z0-9]+),runelength(6|15),required"`
+	PasswordAgain string `json:"password_again" valid:"matches([a-zA-Z0-9]+),runelength(6|15),required"`
 }
