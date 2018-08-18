d48e525ce98dbb8d5436bac4b77bc06a2a85f649
vj861214 <vj861214@gmail.com>
2018-08-17 17:20:04 -0700

    database tables setting not null

--- migrations/00007_create_tickets_table.sql
+++ migrations/00007_create_tickets_table.sql
@@ -1,14 +1,13 @@
 -- +goose Up
 CREATE TABLE tickets (
     ticket_id int NOT NULL AUTO_INCREMENT,
-    inventory_id int,
+    inventory_id int NOT NULL,
     user_id int,
-    token_id int(11),
-    time int(11),
+    time int(11) NOT NULL,
     PRIMARY KEY(ticket_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (inventory_id) REFERENCES inventories(inventory_id),
-    KEY (time, token_id)
+    KEY (time)
 );
 
 -- +goose Down
--- migrations/00008_create_payments_table.sql
+++ migrations/00008_create_payments_table.sql
@@ -1,9 +1,9 @@
 -- +goose Up
 CREATE TABLE payments (
     payment_id int NOT NULL AUTO_INCREMENT,
-    ticket_id int,
-    user_id int,
-    created_time int(11),
+    ticket_id int NOT NULL,
+    user_id int NOT NULL,
+    created_time int(11) NOT NULL,
     deleted_time int(11),
     PRIMARY KEY(payment_id),
     FOREIGN KEY (ticket_id) REFERENCES tickets(ticket_id),
