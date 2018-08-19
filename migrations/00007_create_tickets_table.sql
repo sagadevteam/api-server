53c416fcc060aacb8db0d973505df07a19d5e1e5
Ben Lu <coopermilk123@gmail.com>
2018-08-19 13:18:05 -0700

    Add on_chain column to tickets table

--- migrations/00007_create_tickets_table.sql
+++ migrations/00007_create_tickets_table.sql
@@ -4,10 +4,11 @@ CREATE TABLE tickets (
     inventory_id int NOT NULL,
     user_id int,
     time int(11) NOT NULL,
+    on_chain tinyint(1) NOT NULL DEFAULT '0',
     PRIMARY KEY(ticket_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (inventory_id) REFERENCES inventories(inventory_id),
-    KEY (time)
+    KEY (time, on_chain)
 );
 
 -- +goose Down
