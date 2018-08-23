676e57c83e7710bcc310a4430658dc1ca74d7b19
Ben Lu <coopermilk123@gmail.com>
2018-08-23 14:27:29 -0700

    Add unique index

--- migrations/00005_create_deposits_table.sql
+++ migrations/00005_create_deposits_table.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 CREATE TABLE deposits (
     deposit_id int NOT NULL AUTO_INCREMENT,
-    txhash varchar(80) NOT NULL,
+    txhash varchar(80) NOT NULL UNIQUE,
     address varchar(60) NOT NULL,
     approved tinyint(1) NOT NULL DEFAULT '0',
     PRIMARY KEY(deposit_id),
